362b7e619aa0665cc1e6a9ac13c5bf285efb902e
Koby Fink <finkkoby@icloud.com>
2024-02-11 09:45:50 -0500

    complete lab

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,16 +1,13 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  let counter = {}
   for (let num of arr) {
-    let tracker = {};
-    tracker.num = num;
-    tracker.counter = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] === num) {
-        tracker.counter++;
-      }
-      if (tracker.counter === 2) {
-        return num;
-      }
+    counter[num] = 0;
+  }
+  for (let num of arr) {
+    counter[num]++;
+    if (counter[num] === 2) {
+      return num;
     }
   }
   return -1;
@@ -18,8 +15,8 @@ function findFirstDuplicate(arr) {
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 2");
-  console.log("=>", findFirstDuplicate([2, 2]));
+  console.log("Expecting: 3");
+  console.log("=>", findFirstDuplicate([1, 2, 3, 3, 2]));
 
   console.log("");
 
@@ -32,11 +29,10 @@ module.exports = findFirstDuplicate;
 /* Please add your pseudocode to this file
 And a written explanation of your solution
 
-iterate through each number in an array
-create an object with properties denoting the number and the counter set to 0;
-create a second iteration that iterates through each each number again to check for matches
-  with this method, i will expect at least one match as it iterates over itself
-  each match will increase the counter property value
-  iteration will stop once counter = 2f
-outside of both iterations will return a -1 if nothing else gets return from iterating
+create a counter obj to store our answers
+iterate through each number in the array to define a property for that number that will eventually hold a counter total
+upon declaration of that property, i have it set to zero, that way if there are multplies of the same number, there wont be multiple properties
+I then iterated through the array again and added 1 to matching property of the counter object
+The first key/value pair that === 2 (since each number would match with itself once), it returned that number
+If no number was return from that second iteration, the function by default will return -1
  */
