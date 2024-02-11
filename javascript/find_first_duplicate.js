f467912180a802d44c5b323c8a735726f32ed35d
Koby Fink <finkkoby@icloud.com>
2024-02-11 09:37:07 -0500

    first try

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,11 +1,25 @@
 function findFirstDuplicate(arr) {
   // type your code here
+  for (let num of arr) {
+    let tracker = {};
+    tracker.num = num;
+    tracker.counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === num) {
+        tracker.counter++;
+      }
+      if (tracker.counter === 2) {
+        return num;
+      }
+    }
+  }
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 3");
-  console.log("=>", findFirstDuplicate([2, 1, 3, 3, 2]));
+  console.log("Expecting: 2");
+  console.log("=>", findFirstDuplicate([2, 2]));
 
   console.log("");
 
@@ -15,5 +29,14 @@ if (require.main === module) {
 
 module.exports = findFirstDuplicate;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+/* Please add your pseudocode to this file
+And a written explanation of your solution
+
+iterate through each number in an array
+create an object with properties denoting the number and the counter set to 0;
+create a second iteration that iterates through each each number again to check for matches
+  with this method, i will expect at least one match as it iterates over itself
+  each match will increase the counter property value
+  iteration will stop once counter = 2f
+outside of both iterations will return a -1 if nothing else gets return from iterating
+ */
